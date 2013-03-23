0503c312084bbd9613e95f849c1649c2e5b828c5
Wojciech Gawronski <afronski@gmail.com>
2013-03-23 11:05:39 +0100

    Improving quality of recorded images

--- record.sh
+++ record.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ $# -eq 1 ]; then
-  ffmpeg -f video4linux2 -r 12 -s 640x480 -i /dev/video0 $1.avi
+  ffmpeg -f video4linux2 -r 12 -s 640x480 -i /dev/video0 -qscale 0 $1.avi
 elif [ $# -eq 2 ]; then
-  ffmpeg -f video4linux2 -r $2 -s 640x480 -i /dev/video0 $1.avi
+  ffmpeg -f video4linux2 -r $2 -s 640x480 -i /dev/video0 -qscale 0 $1.avi
 else
   echo "Missing at least file name for output AVI (pass without extensions) :("
   echo "As a second parameter you can pass also a frame rate in Hz ;)"
