5f2a476b3d35f3b0fe3089908a90292245c98bfd
Wojciech Gawronski <afronski@gmail.com>
2013-05-24 16:53:18 +0200

    Tree structure finished

--- domain/tracking/implementations/random-forest-internals-implementation/Tree.hpp
+++ domain/tracking/implementations/random-forest-internals-implementation/Tree.hpp
@@ -18,19 +18,96 @@ template<typename TNode, int MaximumChildNodesCount>
 template<typename TNode, int MaximumChildNodesCount>
   class Tree {
     public:
+      typedef NodeFamily<TNode, MaximumChildNodesCount> PreparedNodeFamily;
+
       TNode& getRoot() const {
-        return const_cast<TNode&>(externalNodesStorage[0]);
+        return *nodes[0];
       }
 
-      typedef NodeFamily<TNode, MaximumChildNodesCount> PreparedNodeFamily;
+      bool hasRoot() const {
+        return !nodes.empty();
+      }
+
+      TNode* getChildNode(const TNode& parentNode, int childNumber) const {
+        for (std::size_t i = 0; i < nodes.size(); ++i) {
+          if (nodes[i] == &parentNode) {
+            return families[i].children[childNumber]->parent;
+          }
+        }
+
+        return 0;
+      }
+
+      bool hasChildNode(const TNode& parentNode, int childNumber) const {
+        for (std::size_t i = 0; i < nodes.size(); ++i) {
+          if (nodes[i] == &parentNode) {
+            return families[i].children[childNumber] != 0;
+          }
+        }
+
+        return false;
+      }
+
+      bool isLeafNode(const TNode& node) const {
+        PreparedNodeFamily* nodeChildren = 0;
+
+        for (std::size_t i = 0; i < nodes.size(); ++i) {
+          if (nodes[i] == &node) {
+            nodeChildren = families[i];
+          }
+        }
+
+        if (nodeChildren == 0) {
+          return false;
+        }
+
+        for (int i = 0; i < MaximumChildNodesCount; ++i) {
+          if (nodeChildren->children[i] != 0) {
+            return false;
+          }
+        }
+
+        return true;
+      }
+
+      void addNode(TNode* node, const TNode* parentNode, int childNumber) {
+        PreparedNodeFamily* nodeFamily = new PreparedNodeFamily();
+        nodeFamily->parent = node;
+
+        if (hasRoot()) {
+          PreparedNodeFamily* parentFamily = 0;
+
+          for (std::size_t i = 0; i < nodes.size(); ++i) {
+            if (nodes[i] == parentNode) {
+              parentFamily = families[i];
+            }
+          }
+
+          parentFamily->children[childNumber] = nodeFamily;
+        }
+
+        nodes.push_back(node);
+        families.push_back(nodeFamily);
+      }
+
+    private:
       typedef std::vector<TNode*> Nodes;
       typedef std::vector<PreparedNodeFamily*> Families;
 
       Nodes nodes;
       Families families;
 
-    private:
-      std::vector<TNode> externalNodesStorage;
+      bool hasNode(const TNode& node) const {
+        for (std::size_t i = 0; i < nodes.size(); ++i) {
+          if (nodes[i] == &node) {
+            return true;
+          }
+        }
+
+        return false;
+      }
+
+      template<typename TNodeInternal, int MaximumChildNodesCountInternal> friend class TreeWalker;
   };
 
 template<typename TNode, int MaximumChildNodesCount>
@@ -50,6 +127,7 @@ template<typename TNode, int MaximumChildNodesCount>
         for (std::size_t i = 0; i < treeStructure.nodes.size(); ++i) {
           if (treeStructure.nodes[i] == &node) {
             currentFamily = treeStructure.families[i];
+            break;
           }
         }
       }
